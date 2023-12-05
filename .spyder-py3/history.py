ba74bcccf7bbf45fd0150ecc4ed7518e798cd7c8
Jan Stein <45586480+janfrederikstein@users.noreply.github.com>
2023-12-05 13:02:17 +0100

    Your commit message

---
+++ .spyder-py3/history.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Wed Sep 13 16:47:25 2023)---
+runfile('/Users/janstein/.spyder-py3/temp.py', wdir='/Users/janstein/.spyder-py3')
+python3 temp.py
